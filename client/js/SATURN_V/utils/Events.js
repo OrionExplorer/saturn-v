c9e53fdf12e809aef013db88378f7f3dd482ee64
Marcin Kelar <orion.exp@gmail.com>
2013-07-20 21:22:46 +0200

    Switching between rocket modules restored.

--- client/js/SATURN_V/utils/Events.js
+++ client/js/SATURN_V/utils/Events.js
@@ -2,12 +2,52 @@ JSMVC.define('SATURN_V.utils.Events', {
 	
 	init : function() {
 		this.onSaturnVLabelClick();
+		this.showApolloSA514ControlPanel();
+		this.showApolloLMControlPanel();
+		this.showApolloCSMControlPanel();
+		this.showHoustonMCControlPanel();
 	},
 
 	onSaturnVLabelClick : function() {
-		var saturnVconfig = document.getElementById('saturnVconfig');
-		saturnVconfig.addEventListener('click', function() {
+		document.getElementById('saturnVconfig').addEventListener('click', function() {
 			SATURN_V.controller.Network.getSaturnVRemoteAddress(true);
 		});
+	},
+
+	showApolloSA514ControlPanel : function() {
+		document.getElementById('showApolloSA514ControlPanel_CSM').addEventListener('click', function() {
+			SATURN_V.controller.MainView.showApolloSA514ControlPanel();
+		});
+		document.getElementById('showApolloSA514ControlPanel_LM').addEventListener('click', function() {
+			SATURN_V.controller.MainView.showApolloSA514ControlPanel();
+		});
+		document.getElementById('showApolloSA514ControlPanel_MC').addEventListener('click', function() {
+			SATURN_V.controller.MainView.showApolloSA514ControlPanel();
+		});
+	},
+
+	showApolloLMControlPanel : function() {
+		document.getElementById('showApolloLMControlPanel_CSM').addEventListener('click', function() {
+			SATURN_V.controller.MainView.showApolloLMControlPanel();
+		});
+	},
+
+	showApolloCSMControlPanel : function() {
+		document.getElementById('showApolloCSMControlPanel_LM').addEventListener('click', function() {
+			SATURN_V.controller.MainView.showApolloCSMControlPanel();
+		});
+		document.getElementById('showApolloCSMControlPanel_MC').addEventListener('click', function() {
+			SATURN_V.controller.MainView.showApolloCSMControlPanel();
+		});
+		document.getElementById('showApolloCSMControlPanel_SA').addEventListener('click', function() {
+			SATURN_V.controller.MainView.showApolloCSMControlPanel();
+		});
+	},
+
+	showHoustonMCControlPanel : function() {
+		document.getElementById('showHoustonMCControlPanel_SA').addEventListener('click', function() {
+			SATURN_V.controller.MainView.showHoustonMCControlPanel();
+		});
+		
 	}
 });
